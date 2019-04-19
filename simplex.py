a3c2359e6dad8f54b4d6e4d8782d5ea3591f3cad
Daniel Kogan <1614745@stud.hs-mannheim.de>
2019-04-19 18:46:42 +0200

    initial commit simplex.py

---
+++ simplex.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+__author__ = "Daniel Kogan"
+
+import logging
+
+class simplex():
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        logging.basicConfig(level=logging.INFO)
+        self.logger.info("Initialized")
+    
+    def initial_tableau(self, c, A, b):
+        tableau = [row[:] + [x] for row, x in zip(A, b)]
+        tableau.append([i for i in c] + [0])
+        return tableau
+
+if __name__ == "__main__":
+    simplex = simplex()
+
+    c = [3, 2]
+    A = [[1,2], [1,-1]]
+    b = [4, 1]
+
+    # schlupfvariablen hinzufügen
+    A[0] += [1,0]
+    A[1] += [0,1]
+    c += [0, 0]
+
+    simplex.logger.info("A matrix: " + str(A))
+    simplex.logger.info("zielfunktion: " + str(c))
+    simplex.logger.info("b vektor: " + str(b))
+
+    # erstes tableau bestimmen
+    tableau = simplex.initial_tableau(c, A, b)
+    simplex.logger.info("Initial tableau:")
+    for row in tableau:
+        print(row)
